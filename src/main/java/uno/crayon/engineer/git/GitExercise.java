341281dd77cc1cd456b0e92e3aa6a3082fc15717
Crayon <hexin.in@qq.com>
2024-01-13 01:31:18 +0800

    two

--- src/main/java/uno/crayon/engineer/git/GitExercise.java
+++ src/main/java/uno/crayon/engineer/git/GitExercise.java
@@ -6,4 +6,8 @@ package uno.crayon.engineer.git;
 public class GitExercise {
 
     public final static String DO_SOMETHING = "asdff";
+    
+    public static void main(String[] args) {
+        System.out.println("asdfasdfasdfasdf");
+    }
 }
