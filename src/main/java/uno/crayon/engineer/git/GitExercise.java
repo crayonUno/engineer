ed3b87da791a9fa026705941f6c25ffce33b684a
Crayon <40671564+crayonUno@users.noreply.github.com>
2024-01-13 15:04:59 +0800

    Update GitExercise.java
    upstream changes

--- src/main/java/uno/crayon/engineer/git/GitExercise.java
+++ src/main/java/uno/crayon/engineer/git/GitExercise.java
@@ -9,5 +9,6 @@ public class GitExercise {
     
     public static void main(String[] args) {
         System.out.println("asdfasdfasdfasdf");
+        System.out.println("upstream changes");
     }
 }
