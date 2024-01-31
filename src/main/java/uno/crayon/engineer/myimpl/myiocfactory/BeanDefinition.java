da756c2cf6765de40634da1d28e311523399baa2
crayonoo <fanghx024@foxmail.com>
2024-01-31 10:53:02 +0800

    feat:A simple BeanFactory impl. copyed from ref. Still needs to learn more about spring concept

---
+++ src/main/java/uno/crayon/engineer/myimpl/myiocfactory/BeanDefinition.java
@@ -0,0 +1,27 @@
+package uno.crayon.engineer.myimpl.myiocfactory;
+
+/**
+ * @author
+ */
+public class BeanDefinition {
+
+    private String beanName;
+
+    private Class beanClass;
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+}
---
+++ src/main/java/uno/crayon/engineer/myimpl/myiocfactory/MyPhoneBean.java
@@ -0,0 +1,11 @@
+package uno.crayon.engineer.myimpl.myiocfactory;
+
+/**
+ * @author
+ */
+public class MyPhoneBean {
+
+    public void queryPhoneInfo(){
+        System.out.println("iPhone15.");
+    }
+}
